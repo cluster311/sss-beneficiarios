3d373d9e44c6439cd14425f7a83523ea2e34a7a1
Andres Vazquez <andres@data99.com.ar>
2020-08-17 20:22:32 -0300

    Fix lib

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
      name='sss_beneficiarios_hospitales',
-     version='0.8.113',
+     version='0.8.114',
      license='MIT',
      entry_points={
         },
